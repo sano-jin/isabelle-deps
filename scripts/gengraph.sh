c263a60e54002485e26c4c6da5cd1dfeb241a11f
sano <sano@ueda.info.waseda.ac.jp>
2026-04-04 00:03:28 +0900

    make the tool as a cli tool.

--- scripts/gengraph.sh
+++ scripts/gengraph.sh
@@ -2,7 +2,6 @@
 
 set -eux
 
-poetry install --no-root
-poetry run python ../main.py $1 > depgraph.mmd
+poetry run isabelle-deps $1 > depgraph.mmd
 mmdc -i depgraph.mmd -o depgraph.png -s 10
 # mmdc -i depgraph.mmd -o depgraph.svg
