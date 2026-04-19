08f751d8cbe857b355e77cb4d08e56be8a15580a
sano <sano@ueda.info.waseda.ac.jp>
2026-04-19 13:39:05 +0900

    add another example.

---
+++ demo/InductTest.thy
@@ -0,0 +1,145 @@
+chapter\<open>An example showing the use of structural inductions.\<close>
+
+theory InductTest
+imports Main begin
+
+type_synonym var = nat
+
+datatype exp =
+  Var var
+| App "exp" "exp"
+| Lam "exp"
+
+
+text \<open>
+Free variables of an expression.
+The constructor Lam hides a variable with an index 0 in its sub-expression,
+and all other free variables are shifted down by one.
+\<close>
+primrec FV :: "exp => var set"
+where
+  "FV (Var var) = {var}"
+| "FV (App e1 e2) = FV e1 \<union> FV e2"
+| "FV (Lam e) = {n. Suc n \<in> FV e}"
+
+
+text \<open>
+Free variable replacement.
+Note that this is designed not to change any local variable.
+\<close>
+primrec
+  vmap :: "(var => var) => exp => exp"
+where
+  "vmap f (Var xs) = Var (f xs)"
+| "vmap f (App e1 e2) = App (vmap f e1) (vmap f e2)"
+| "vmap f (Lam e) = Lam (vmap (\<lambda>x. if x = 0 then x else (f (x - 1) + 1)) e)"
+
+
+text \<open>
+Composition of free variable replacements.
+\<close>
+lemma vmap_compose:
+  "vmap f (vmap g exp) = vmap (f \<circ> g) exp"
+proof (induct exp arbitrary: f g)
+  case (Var x2)
+  then show ?case
+    by auto
+next
+  case (App exp1 exp2)
+  then show ?case
+    by auto
+next
+  case (Lam exp)
+  have P2: "(\<lambda>x. if x = 0 then x else f (x - 1) + 1) \<circ>
+    (\<lambda>x. if x = 0 then x else g (x - 1) + 1) =
+    (\<lambda>x. if x = 0 then x else f (g (x - 1)) + 1)"
+    using Suc_eq_plus1 Zero_not_Suc diff_Suc_1 by auto
+  show ?case
+    apply auto
+    using Lam P2 by presburger
+qed
+
+
+text \<open>
+A helper lemma...
+\<close>
+lemma helper_lemma_for_the_generalised_lemma:
+  assumes "\<forall>x \<in> FV (Lam exp). (k <= x --> n <= x)"
+  shows "\<forall>x \<in> FV exp. (k + 1 <= x --> n + 1 <= x)"
+using assms
+apply auto
+using Suc_le_D by auto
+
+
+text \<open>
+A ``generalised'' lemma to show the next lemma.
+\<close>
+lemma generalised_lemma_for_shift_down_and_up:
+  assumes "\<forall>x \<in> FV exp. (k <= x --> n <= x)"
+  shows "vmap (\<lambda>x. if k <= x then x - n + n else x) exp = exp"
+using assms
+proof (induct exp arbitrary: n k)
+  case (Var xs)
+  then show ?case
+    apply auto
+    done
+next
+  case (App x1 x2)
+  then show ?case
+    apply auto
+    done
+next
+  case (Lam exp)
+  then have H: "\<forall>x \<in> FV exp. (k + 1 \<le> x --> n + 1 \<le> x)"
+    using helper_lemma_for_the_generalised_lemma by blast
+  have f: "\<forall>x.
+    (if x = 0 then x else (if k \<le> x - 1 then x - 1 - n + n else x - 1) + 1) =
+    (if (k + 1) \<le> x then x - (n + 1) + (n + 1) else x)"
+    by auto
+  then show ?case
+    apply auto
+    using H Lam.hyps Suc_eq_plus1 by presburger
+qed
+
+
+text \<open>
+A slightly generalised lemma to show the final theorem.
+\<close>
+lemma shift_down_and_up_if_less_than_n_notin:
+  assumes "\<forall>x \<in> FV exp. n <= x"
+  shows "vmap (\<lambda>x. x - n + n) exp = exp"
+proof -
+  have "\<forall>x \<in> FV exp. (0 <= x --> n <= x)"
+    by (simp add: assms)
+  then have "vmap (\<lambda>x. if 0 <= x then x - n + n else x) exp = exp"
+    using generalised_lemma_for_shift_down_and_up by blast
+  then have "vmap (\<lambda>x. x - n + n) exp = exp"
+    by simp
+  then show ?thesis .
+qed
+
+
+text \<open>
+The final theorem.
+If 0 is not in the free variables of an expression,
+then shifting down and up all the variables in the expression
+does not change the expression.
+\<close>
+theorem shift_down_and_up_if_0_notin:
+  assumes "0 \<notin> FV exp"
+  shows "vmap Suc (vmap (\<lambda>x. x - 1) exp) = exp"
+proof -
+  have "\<forall>x \<in> FV exp. 1 <= x"
+    using assms not_less_eq_eq by auto
+  then have "vmap (\<lambda>x. x - 1 + 1) exp = exp"
+    using shift_down_and_up_if_less_than_n_notin by blast
+  then have "vmap ((\<lambda>x. x + 1) \<circ> (\<lambda>x. x - 1)) exp = exp"
+    by (simp add: comp_def)
+  then have "vmap (\<lambda>x. x + 1) (vmap (\<lambda>x. x - 1) exp) = exp"
+    by (simp add: vmap_compose)
+  then show ?thesis
+    by simp
+qed
+
+end
+
